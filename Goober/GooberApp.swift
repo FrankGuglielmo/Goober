723d4d5e013309f5e17f1c3476cd252c19c3dda0
Frank Guglielmo <frankieg1610@gmail.com>
2023-11-26 00:31:38 -0800

    Initial Commit

---
+++ Goober/GooberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GooberApp.swift
+//  Goober
+//
+//  Created by Frank Guglielmo on 11/26/23.
+//
+
+import SwiftUI
+
+@main
+struct GooberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
